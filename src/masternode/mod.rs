4325d449da7d5aa8b44ee4c9e3e945cf0c1882b5
pankcuf <rastacake@gmail.com>
2022-04-14 20:02:33 +0300

    [0.1.1: bytes_decodable impl]

--- src/masternode/mod.rs
+++ src/masternode/mod.rs
@@ -1,3 +1,7 @@
+use byte::{BytesExt, LE};
+use dash_spv_primitives::crypto::byte_util::BytesDecodable;
+use dash_spv_primitives::impl_bytes_decodable;
+
 pub mod llmq_entry;
 pub mod masternode_list;
 pub mod masternode_entry;
@@ -5,3 +9,5 @@ pub mod masternode_entry;
 pub use self::llmq_entry::LLMQEntry;
 pub use self::masternode_entry::MasternodeEntry;
 pub use self::masternode_list::MasternodeList;
+
+impl_bytes_decodable!(MasternodeEntry);
--- src/tx/mod.rs
+++ src/tx/mod.rs
@@ -1,2 +1,17 @@
+use byte::{BytesExt, LE};
+use dash_spv_primitives::crypto::byte_util::BytesDecodable;
+use dash_spv_primitives::impl_bytes_decodable_lt;
+
 pub mod coinbase_transaction;
 pub mod transaction;
+
+pub use self::transaction::Transaction;
+pub use self::transaction::TransactionInput;
+pub use self::transaction::TransactionOutput;
+pub use self::transaction::TransactionType;
+pub use self::coinbase_transaction::CoinbaseTransaction;
+
+impl_bytes_decodable_lt!(TransactionInput);
+impl_bytes_decodable_lt!(TransactionOutput);
+impl_bytes_decodable_lt!(Transaction);
+impl_bytes_decodable_lt!(CoinbaseTransaction);
