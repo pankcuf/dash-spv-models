e4fa90e9138d66fc650efeb800883ca8131d5f23
pankcuf <rastacake@gmail.com>
2022-04-14 19:33:08 +0300

    [migrated from masternode-diff-processor]

---
+++ src/common/chain_type.rs
@@ -0,0 +1,6 @@
+#[derive(Clone, Copy, Debug)]
+pub enum ChainType {
+    MainNet = 0,
+    TestNet = 1,
+    DevNet = 2,
+}
---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+pub mod common;
+pub mod masternode;
+pub mod tx;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
