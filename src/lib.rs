d1d285821cc4a00e167b3a287e91b2600f4b1a83
pankcuf <rastacake@gmail.com>
2022-04-15 18:41:37 +0300

    [add core v0.18 stuff]

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod common;
+pub mod llmq;
 pub mod masternode;
 pub mod tx;
 
---
+++ src/llmq/mod.rs
@@ -0,0 +1,7 @@
+pub mod mn_list_diff;
+pub mod rotation_info;
+pub mod snapshot;
+
+pub use self::mn_list_diff::MNListDiff;
+pub use self::rotation_info::LLMQRotationInfo;
+pub use self::snapshot::LLMQSnapshot;
