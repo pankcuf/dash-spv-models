51be4c2085e2cee7396cb1735f973cca2c319a03
pankcuf <rastacake@gmail.com>
2022-05-05 12:59:31 +0300

    [remove commented]

--- src/llmq/rotation_info.rs
+++ src/llmq/rotation_info.rs
@@ -14,7 +14,6 @@ pub struct LLMQRotationInfo<'a> {
     pub mn_list_diff_at_h_3c: MNListDiff<'a>,
     pub mn_list_diff_at_h_4c: Option<MNListDiff<'a>>, // exist only if extra_share is true
     pub extra_share: bool,
-    // pub last_quorum_hash_per_index: VarInt,
     pub last_quorum_hash_per_index: Vec<UInt256>,
     pub quorum_snapshot_list: Vec<LLMQSnapshot<'a>>,
     pub mn_list_diff_list: Vec<MNListDiff<'a>>,
