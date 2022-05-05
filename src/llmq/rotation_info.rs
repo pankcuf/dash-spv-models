e04861afc75ee62bbe41ece4c532b33033dab676
pankcuf <rastacake@gmail.com>
2022-05-05 12:54:31 +0300

    [0.1.4: add 'lastQuorumHashPerIndex' for LLMQRotation]

--- src/llmq/rotation_info.rs
+++ src/llmq/rotation_info.rs
@@ -1,3 +1,4 @@
+use dash_spv_primitives::crypto::byte_util::UInt256;
 use crate::llmq::{LLMQSnapshot, MNListDiff};
 
 #[derive(Debug)]
@@ -13,4 +14,8 @@ pub struct LLMQRotationInfo<'a> {
     pub mn_list_diff_at_h_3c: MNListDiff<'a>,
     pub mn_list_diff_at_h_4c: Option<MNListDiff<'a>>, // exist only if extra_share is true
     pub extra_share: bool,
+    // pub last_quorum_hash_per_index: VarInt,
+    pub last_quorum_hash_per_index: Vec<UInt256>,
+    pub quorum_snapshot_list: Vec<LLMQSnapshot<'a>>,
+    pub mn_list_diff_list: Vec<MNListDiff<'a>>,
 }
