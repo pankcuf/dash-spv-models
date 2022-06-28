d62eb4d640edc6e3346ad817822ac67c52cf0fca
pankcuf <rastacake@gmail.com>
2022-06-28 14:38:25 +0300

    [0.1.14: make llmqindexedhash copyable]

--- src/llmq/llmq_typed_hash.rs
+++ src/llmq/llmq_typed_hash.rs
@@ -1,13 +1,13 @@
 use dash_spv_primitives::crypto::UInt256;
 use crate::common::LLMQType;
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 pub struct LLMQTypedHash {
     pub r#type: LLMQType,
     pub hash: UInt256,
 }
 
-#[derive(Debug, Ord, PartialOrd, Eq, PartialEq)]
+#[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
 pub struct LLMQIndexedHash {
     pub index: u32,
     pub hash: UInt256,
