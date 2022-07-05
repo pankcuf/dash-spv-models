bc71c595d415d3af70b45fd693acdb0b6324ab5f
pankcuf <rastacake@gmail.com>
2022-07-05 13:38:37 +0300

    [update models]

--- src/llmq/rotation_info.rs
+++ src/llmq/rotation_info.rs
@@ -3,10 +3,10 @@ use crate::masternode::LLMQEntry;
 
 #[derive(Debug)]
 pub struct LLMQRotationInfo<'a> {
-    pub snapshot_at_h_c: LLMQSnapshot<'a>,
-    pub snapshot_at_h_2c: LLMQSnapshot<'a>,
-    pub snapshot_at_h_3c: LLMQSnapshot<'a>,
-    pub snapshot_at_h_4c: Option<LLMQSnapshot<'a>>, // exist only if extra_share is true
+    pub snapshot_at_h_c: LLMQSnapshot,
+    pub snapshot_at_h_2c: LLMQSnapshot,
+    pub snapshot_at_h_3c: LLMQSnapshot,
+    pub snapshot_at_h_4c: Option<LLMQSnapshot>, // exist only if extra_share is true
     pub mn_list_diff_tip: MNListDiff<'a>,
     pub mn_list_diff_at_h: MNListDiff<'a>,
     pub mn_list_diff_at_h_c: MNListDiff<'a>,
@@ -15,6 +15,6 @@ pub struct LLMQRotationInfo<'a> {
     pub mn_list_diff_at_h_4c: Option<MNListDiff<'a>>, // exist only if extra_share is true
     pub extra_share: bool,
     pub last_quorum_per_index: Vec<LLMQEntry>,
-    pub quorum_snapshot_list: Vec<LLMQSnapshot<'a>>,
+    pub quorum_snapshot_list: Vec<LLMQSnapshot>,
     pub mn_list_diff_list: Vec<MNListDiff<'a>>,
 }
--- src/llmq/snapshot.rs
+++ src/llmq/snapshot.rs
@@ -5,16 +5,16 @@ use dash_spv_primitives::hashes::hex::ToHex;
 use crate::common::LLMQSnapshotSkipMode;
 
 #[derive(Clone)]
-pub struct LLMQSnapshot<'a> {
+pub struct LLMQSnapshot {
     // The bitset of nodes already in quarters at the start of cycle at height n
     // (masternodeListSize + 7)/8
-    pub member_list: &'a [u8],
+    pub member_list: Vec<u8>,
     // Skiplist at height n
     pub skip_list: Vec<u32>,
     //  Mode of the skip list
     pub skip_list_mode: LLMQSnapshotSkipMode,
 }
-impl<'a> std::fmt::Debug for LLMQSnapshot<'a> {
+impl<'a> std::fmt::Debug for LLMQSnapshot {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("LLMQSnapshot")
             .field("member_list", &self.member_list.to_hex())
@@ -23,22 +23,22 @@ impl<'a> std::fmt::Debug for LLMQSnapshot<'a> {
             .finish()
     }
 }
-impl<'a> TryRead<'a, Endian> for LLMQSnapshot<'a> {
+impl<'a> TryRead<'a, Endian> for LLMQSnapshot {
     fn try_read(bytes: &'a [u8], _endian: Endian) -> byte::Result<(Self, usize)> {
         let offset = &mut 0;
         let member_list_size = bytes.read_with::<VarInt>(offset, LE)?.0 as usize;
-        let member_list = bytes.read_with(offset, Bytes::Len((member_list_size + 7) / 8))?;
+        let member_list: &[u8] = bytes.read_with(offset, Bytes::Len((member_list_size + 7) / 8))?;
         let skip_list_mode = bytes.read_with::<LLMQSnapshotSkipMode>(offset, LE)?;
         let skip_list_size = bytes.read_with::<u16>(offset, LE)? as usize;
         let mut skip_list = Vec::with_capacity(skip_list_size);
         for _i in 0..skip_list_size {
             skip_list.push(bytes.read_with::<u32>(offset, LE)?);
         }
-        Ok((Self { member_list, skip_list, skip_list_mode }, *offset))
+        Ok((Self { member_list: member_list.to_vec(), skip_list, skip_list_mode }, *offset))
     }
 }
 
-impl<'a> LLMQSnapshot<'a> {
+impl LLMQSnapshot {
     pub fn length(&self) -> usize {
         self.member_list.len() + 1 + 2 + self.skip_list.len() * 2
     }
