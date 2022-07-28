bd15b997594b730235b644aa6b1ef2ad8b3234f5
pankcuf <rastacake@gmail.com>
2022-07-28 16:39:32 +0300

    [remove merkle tree logs]

--- src/common/merkle_tree.rs
+++ src/common/merkle_tree.rs
@@ -24,12 +24,10 @@ pub struct MerkleTree<'a> {
 
 impl<'a> MerkleTree<'a> {
     pub fn has_root(&self, desired_merkle_root: UInt256) -> bool {
-        println!("MerkleTree.has_root: {} tree: [ {}, {:?}, {:?} ]", desired_merkle_root, self.tree_element_count, self.hashes, self.flags);
         if self.tree_element_count == 0 {
             return true;
         }
         if let Some(root) = self.merkle_root() {
-            println!("MerkleTree finding root....: {} == {}", root, desired_merkle_root);
             if root == desired_merkle_root {
                 return true;
             }
@@ -61,15 +59,14 @@ impl<'a> MerkleTree<'a> {
         &self, hash_idx: &mut i32, flag_idx: &mut i32,
         depth: i32, leaf: LL, branch: BL) -> Option<UInt256> {
         let flags_length = self.flags.len() as i32;
-        let hashes_length = 32*self.hashes.len() as i32;
-        if *flag_idx / 8 >= flags_length || (*hash_idx + 1) * 32 > hashes_length {
+        let hashes_length = self.hashes.len() as i32;
+        if *flag_idx / 8 >= flags_length || *hash_idx >= hashes_length {
             return leaf(None, false);
         }
         let flag = self.flags[(*flag_idx / 8) as usize] & (1 << (*flag_idx % 8)) != 0;
         *flag_idx += 1;
         if !flag || depth == ceil_log2(self.tree_element_count as i32) {
             let hash = self.hashes.get(*hash_idx as usize).copied();
-            println!("MerkleTree walk_hash_idx....: {:?}", hash);
             *hash_idx += 1;
             return leaf(hash, flag);
         }
