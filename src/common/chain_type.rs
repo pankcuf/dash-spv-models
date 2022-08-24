8ecbfc106b8e1104d2aeaac065fe917e022b8748
pankcuf <rastacake@gmail.com>
2022-08-24 19:07:09 +0300

    [add genesis_hash for ChainType]

--- src/common/chain_type.rs
+++ src/common/chain_type.rs
@@ -1,3 +1,7 @@
+use dash_spv_primitives::crypto::byte_util::Reversable;
+use dash_spv_primitives::crypto::UInt256;
+use dash_spv_primitives::hashes::hex::FromHex;
+
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
 pub enum ChainType {
     MainNet = 0,
@@ -8,5 +12,13 @@ pub enum ChainType {
 impl ChainType {
     pub fn is_mainnet(&self) -> bool {
         *self == ChainType::MainNet
+    }
+
+    pub fn genesis_hash(&self) -> UInt256 {
+        match self {
+            ChainType::MainNet => UInt256::from_hex("00000ffd590b1485b3caadc19b22e6379c733355108f107a430458cdf3407ab6"),
+            ChainType::TestNet => UInt256::from_hex("00000bafbc94add76cb75e2ec92894837288a481e5c005f6563d91623bf8bc2c"),
+            ChainType::DevNet => UInt256::from_hex("00000bafbc94add76cb75e2ec92894837288a481e5c005f6563d91623bf8bc2c"),
+        }.unwrap().reversed()
     }
 }
