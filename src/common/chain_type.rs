a6e3a414bd86a61ad9eb26b270b502bd4eda323a
pankcuf <rastacake@gmail.com>
2022-09-21 17:12:46 +0400

    [enhance chaintype]

--- src/common/chain_type.rs
+++ src/common/chain_type.rs
@@ -3,11 +3,38 @@ use dash_spv_primitives::crypto::UInt256;
 use dash_spv_primitives::hashes::hex::FromHex;
 use crate::common::LLMQType;
 
+pub trait IHaveChainSettings {
+    fn genesis_hash(&self) -> UInt256;
+    fn is_llmq_type(&self) -> LLMQType;
+    fn isd_llmq_type(&self) -> LLMQType;
+    fn chain_locks_type(&self) -> LLMQType;
+    fn platform_type(&self) -> LLMQType;
+}
+
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
 pub enum ChainType {
-    MainNet = 0,
-    TestNet = 1,
-    DevNet = 2,
+    MainNet,
+    TestNet,
+    DevNet(DevnetType),
+}
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
+pub enum DevnetType {
+    JackDaniels,
+    Devnet333,
+}
+
+impl DevnetType {
+    pub fn identifier(&self) -> String {
+        match self {
+            DevnetType::JackDaniels => "jack-daniels".to_string(),
+            DevnetType::Devnet333 => "333".to_string(),
+        }
+    }
+
+    pub fn version(&self) -> u16 {
+        1
+    }
 }
 
 impl ChainType {
@@ -15,27 +42,73 @@ impl ChainType {
         *self == ChainType::MainNet
     }
 
-    pub fn genesis_hash(&self) -> UInt256 {
+}
+
+impl IHaveChainSettings for ChainType {
+
+    fn genesis_hash(&self) -> UInt256 {
         match self {
-            ChainType::MainNet => UInt256::from_hex(
-                "00000ffd590b1485b3caadc19b22e6379c733355108f107a430458cdf3407ab6",
-            ),
-            ChainType::TestNet => UInt256::from_hex(
-                "00000bafbc94add76cb75e2ec92894837288a481e5c005f6563d91623bf8bc2c",
-            ),
-            ChainType::DevNet => UInt256::from_hex(
-                "00000bafbc94add76cb75e2ec92894837288a481e5c005f6563d91623bf8bc2c",
-            ),
+            ChainType::MainNet => UInt256::from_hex("00000ffd590b1485b3caadc19b22e6379c733355108f107a430458cdf3407ab6").unwrap().reversed(),
+            ChainType::TestNet => UInt256::from_hex("00000bafbc94add76cb75e2ec92894837288a481e5c005f6563d91623bf8bc2c").unwrap().reversed(),
+            ChainType::DevNet(devnet_type) => devnet_type.genesis_hash(),
         }
-        .unwrap()
-        .reversed()
     }
 
-    pub fn isd_llmq_type(&self) -> LLMQType {
+    fn is_llmq_type(&self) -> LLMQType {
+        match self {
+            ChainType::MainNet => LLMQType::Llmqtype50_60,
+            ChainType::TestNet => LLMQType::Llmqtype50_60,
+            ChainType::DevNet(devnet_type) => devnet_type.is_llmq_type(),
+        }
+    }
+
+    fn isd_llmq_type(&self) -> LLMQType {
         match self {
             ChainType::MainNet => LLMQType::Llmqtype60_75,
             ChainType::TestNet => LLMQType::Llmqtype60_75,
-            ChainType::DevNet => LLMQType::LlmqtypeDevnetDIP0024,
+            ChainType::DevNet(devnet_type) => devnet_type.isd_llmq_type(),
         }
+    }
+
+    fn chain_locks_type(&self) -> LLMQType {
+        match self {
+            ChainType::MainNet => LLMQType::Llmqtype400_60,
+            ChainType::TestNet => LLMQType::Llmqtype50_60,
+            ChainType::DevNet(devnet_type) => devnet_type.chain_locks_type(),
+        }
+    }
+
+    fn platform_type(&self) -> LLMQType {
+        match self {
+            ChainType::MainNet => LLMQType::Llmqtype100_67,
+            ChainType::TestNet => LLMQType::Llmqtype100_67,
+            ChainType::DevNet(devnet_type) => devnet_type.platform_type(),
+        }
+
+    }
+}
+
+impl IHaveChainSettings for DevnetType {
+
+    fn genesis_hash(&self) -> UInt256 {
+        UInt256::from_hex("00000bafbc94add76cb75e2ec92894837288a481e5c005f6563d91623bf8bc2c")
+            .unwrap()
+            .reversed()
+    }
+
+    fn is_llmq_type(&self) -> LLMQType {
+        LLMQType::LlmqtypeDevnetDIP0024
+    }
+
+    fn isd_llmq_type(&self) -> LLMQType {
+        LLMQType::LlmqtypeDevnetDIP0024
+    }
+
+    fn chain_locks_type(&self) -> LLMQType {
+        LLMQType::LlmqtypeDevnet
+    }
+
+    fn platform_type(&self) -> LLMQType {
+        LLMQType::LlmqtypeDevnet
     }
 }
